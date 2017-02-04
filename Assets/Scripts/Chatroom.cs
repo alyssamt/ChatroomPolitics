dd7763ab697fe00dddb9e8a9983cdaf3e5af9e60
Alyssa Tan <alyssamt@uci.edu>
2017-02-03 20:23:14 -0800

    First commit

---
+++ Assets/Scripts/Chatroom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chatroom : MonoBehaviour {
+
+	private Text chatroom;
+
+	// Use this for initialization
+	void Start () {
+		chatroom = GetComponent<Text> ();
+		chatroom.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PostMessage(string poster, string message) {
+		chatroom.text += string.Format ("[{0}] {1}: {2}\n", GameManager.time, poster, message);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static string time;
+
+	public Chatroom chatroom;
+
+	// Use this for initialization
+	void Start () {
+		time = "12:00";
+		chatroom.PostMessage ("Shithead", "Hey shitheads");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
